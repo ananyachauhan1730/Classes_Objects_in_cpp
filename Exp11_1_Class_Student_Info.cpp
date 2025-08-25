1821f94bb592fd4e5dee7f29b90174376712b1e9
ananyachauhan1730 <chauhanananya603@gmail.com>
2025-08-25 14:49:15 +0530

    Create Exp11_1_Class_Student_Info.cpp

---
+++ Exp11_1_Class_Student_Info.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+class Student {
+public:
+    string name;
+    string branch;
+    string subject;
+    string year;
+    float resut;
+};
+int main(){
+    Student s1;
+    s1.name = "Ananya Chauhan";
+    s1.branch = "Electronics & Telecomunication Engineering";
+    s1.subject = "C++ & Data Structures";
+    s1.year = "Second Year";
+    s1.resut = 8;
+    cout << "Name: " << s1.name << endl;
+    cout << "Branch: " << s1.branch << endl;
+    cout << "Subject: " << s1.subject << endl;
+    cout << "Year: " << s1.year << endl;
+    cout << "Result: " << s1.resut << endl << endl;
+   
+    return 0;
+}
+
+/*
+Output:
+Name: Ananya Chauhan 
+Branch: Electronics & Telecomunication Engineering
+Subject: C++ & Data Structures
+Year: Second Year
+Result: 8
+*/
